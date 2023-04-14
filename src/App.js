04f586083272d2f4c4acefd16fcf13b105ec15c0
Hemanth Kalyana Sundaram <hks@Hemanths-MacBook-Pro.local>
2023-04-14 16:49:58 -0500

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './App.css'
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    axios.get('https://clickchaser-hk97.hemanth-ks97.repl.co/count')
+      .then(res => {
+        setCount(res.data.count);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }, []);
+
+  const handleIncrement = () => {
+    axios.post('https://clickchaser-hk97.hemanth-ks97.repl.co/increment')
+      .then(res => {
+        setCount(res.data.count);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p id="counter">{count}</p>
+      <button onClick={handleIncrement}>Increment</button>
     </div>
   );
 }
